f7d1759a50b19bb17e84d96f1b548adfbbc828af
victoria.rey <victoria.rey@estudiantes.utec.edu.uy>
2026-06-04 23:33:18 -0300

    feat: integrate Outfit font family and update styles for welcome screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,35 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { 
+  useFonts, 
+  Outfit_300Light,    
+  Outfit_400Regular, 
+  Outfit_600SemiBold, 
+  Outfit_700Bold,
+  Outfit_900Black
+} from '@expo-google-fonts/outfit';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    Outfit_300Light,
+    Outfit_400Regular,
+    Outfit_600SemiBold,
+    Outfit_700Bold,
+    Outfit_900Black
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
